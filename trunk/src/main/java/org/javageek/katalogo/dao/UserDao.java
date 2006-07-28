9a75e8bd19e52bf02cfe2af2ccebe3d83f383976
guillermo.castro <guillermo.castro@1400c883-9719-0410-8cb2-bf74317c8d72>
2006-07-28 17:16:49 +0000

    removed sources from the wrong place.

--- trunk/src/main/java/org/javageek/katalogo/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package org.javageek.katalogo.dao;
-
-import org.javageek.katalogo.model.User;
-
-public interface UserDao extends BaseDao<User>{
-    User authenticate(String email, String password);
-
-    User findByEmail(String address);
-}
--- trunk/src/main/java/org/javageek/katalogo/model/Persistent.java
+++
@@ -1,9 +0,0 @@
-package org.javageek.katalogo.model;
-
-import java.io.Serializable;
-
-public interface Persistent extends Serializable {
-
-    Long getId();
-    void setId(Long id);
-}
